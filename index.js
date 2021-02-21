d67573110058e2a510a022d89477f0e495e91a41
abeer ahmad <abeerahmad204@gmail.com>
2021-02-21 19:57:07 +0500

    first class

---
+++ index.js
@@ -0,0 +1,43 @@
+
+// alert("abeer Ahmad asjdklas jdkl")
+
+var name = "abeer Ahmad asjdklas jdkl";
+
+console.log(name)
+var num = "asudaj sdhjasdh";
+var num2 = 12
+console.log(typeof (num))
+console.log(typeof (num2))
+
+
+var total = num2 + 12
+// console.log(total)
+// console.log(typeof(total))
+var a = 12;
+// var b= 20;
+
+
+var b = ++a
+
+console.log(b)
+console.log(a)
+
+var var1 = 12
+var var2 = 'sameer '
+// var var3 = `abeer is not here.`
+
+let concated = ` ${var1} is teaching ${var2}`
+console.log(concated)
+
+
+if (var1 == '12' && var2 == 'sameer ') {
+    if (var1 === '12') {
+        console.log('here')
+    }else{
+        console.log('error')
+    }
+
+} else {
+    console.log('condition is false')
+}
+
